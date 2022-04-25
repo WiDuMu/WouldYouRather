a18432fabf5ef41a9b6debe72e864c1152ff8b5d
wdunc <wduncanmurphy@gmail.com>
2022-04-24 23:54:22 -0500

    update

---
+++ wouldyourather/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app">
+    <h1>Would you rather... </h1>
+    <would-you-rather v-bind:question="wyrQuestion" 
+    v-bind:answer1="wyrAnswer1" 
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"></would-you-rather>
+
+    <p>{{userSelectionMessage}}</p>
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather take a coding class or an art class?',
+      wyrAnswer1: 'Coding all the way!',
+      wyrAnswer2: "I prefer to be 'creative'.",
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks, you chose ${choice}`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: blueviolet;
+  /*I seek to provide the worst UI ever */
+}
+</style>
